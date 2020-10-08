b4ec1ae9ae647c14eeb2f18df937dca2b29a94fb
Blake Bennett <s532542@nwmissouri.edu>
2020-10-08 14:35:10 -0500

    used html with my node server

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const http = require('http');
-
+const fs = require('fs');
 const hostname = process.env.hostname||'0.0.0.0';
 const port = process.env.PORT||3002;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('My name is Blake Bennett\n')
-  res.write('Thank you for accessing my simple web server I created!\n')
-  res.write('Follow my README.md file if you want to create your own web server\n')
-  res.end('Until next time!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+fs.readFile('./index.html', function (err, html) {
+  if (err) {
+      throw err; 
+  }       
+  http.createServer(function(request, response) {  
+      response.writeHeader(200, {"Content-Type": "text/html"});  
+      response.write(html);  
+      response.end();  
+      console.log(`Server running at http://${hostname}:${port}/`);
+  }).listen(port, hostname);
 });
